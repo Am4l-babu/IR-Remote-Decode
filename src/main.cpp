299782ea9ca89a0a6fcb105dbdd0925d027db590
Amal Babu <amalbabu34767@gmail.com>
2026-02-23 00:19:31 +0530

    security: Move WiFi credentials to gitignored config file with template

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,9 @@
 #include <ArduinoJson.h>
 #include <LittleFS.h>
 #include "learn_page.h"
+#include "wifi_credentials.h"
 
 // ==================== CONFIGURATION ====================
-const char* WIFI_SSID     = "Keralavision@1994";
-const char* WIFI_PASSWORD = "babu7362";
-
 const uint16_t IR_RECV_PIN = 2;  // GPIO2 = D4 on ESP8266
 const uint16_t CAPTURE_BUFFER_SIZE = 1024;
 const uint8_t  TIMEOUT = 50;
